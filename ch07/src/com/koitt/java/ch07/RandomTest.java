0b8faab517fff61944268a58b42c0bb7c3d6293d
KOITT <KOITT@KOITT-PC13>
2018-01-08 17:39:43 +0900

    ch07 p.271까지

--- ch07/src/com/koitt/java/ch07/RandomTest.java
+++ ch07/src/com/koitt/java/ch07/RandomTest.java
@@ -7,14 +7,19 @@ public class RandomTest {
 		Random rd = new Random();
 		// Random rd = new Random(45);
 		
-		for (int i=0; i<6; i++) 
+		for (int i=0; i<6; i++) {
 			System.out.print(rd.nextInt() + " ");
+		}	
 		System.out.println();
-		for (int i=0; i<6; i++) //1 ~ 45 사이
-			System.out.print((rd.nextInt(44)+1) + " ");
+		
+		for (int i=0; i<6; i++) { //원래는 0~44사이 근데 +1을 붙였으니까 1부터 45?
+			System.out.print((rd.nextInt(44)+1) + " "); // 만약 +1이 없다면 0부터 출력됐을것
+		}	
 		System.out.println();
-		for (int i=0; i<6; i++) //0 ~ 1 사이
+		
+		for (int i=0; i<6; i++) { //0 ~ 1 사이의 더블형 숫자들을 랜덤으로 출력
 			System.out.printf("%.2f ", rd.nextDouble());  
+		}	
 		System.out.println();
 	}
 
---
+++ ch07/src/com/koitt/java/ch07/StringTokenizerTest.java
@@ -0,0 +1,44 @@
+package com.koitt.java.ch07;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerTest {
+	
+	public static void main(String[] args) {
+		
+		String str = "public static void main(String[] args) {";
+		StringTokenizer st1 = new StringTokenizer(str); //기본 분리자 "공백,\t\n\r\f":
+		printToken(st1);
+		
+		StringTokenizer st2 = new StringTokenizer(str, " ()"); // 분리자에서 (,) 3개를 추가함
+		printToken(st2);
+		
+		StringTokenizer st3 = new StringTokenizer(str, " ()[]{");
+		printToken(st3);
+		
+		
+		StringTokenizer st4 = new StringTokenizer(str, "()[]{", true); //토큰을 분리자에 포함 
+		printToken(st4);
+		
+		StringTokenizer st5 = new StringTokenizer(str); //기본 분리자 " \t\n\r\f":
+		printToken(st5, "()");
+	}
+	
+	public static void printToken(StringTokenizer st) {
+		System.out.println("토큰 수: " + st.countTokens());
+		int cnt = 0;
+		while (st.hasMoreTokens()) {
+			System.out.print(++cnt + ". " + st.nextToken() + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void printToken(StringTokenizer st, String delim) {
+		int cnt = 0;
+		while (st.hasMoreTokens()) {
+			System.out.print(++cnt + ". " + st.nextToken(delim) + " ");
+		}
+		System.out.println();
+	}
+
+}
