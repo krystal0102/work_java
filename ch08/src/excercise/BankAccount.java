c9f97c23bb427414ac1beeab97333c35f32e0bfe
KOITT <KOITT@KOITT-PC13>
2018-01-17 17:50:02 +0900

    ch08 프로그래밍 연습 2번 진행중

--- ch08/src/excercise/BankAccount.java
+++ ch08/src/excercise/BankAccount.java
@@ -1,5 +1,24 @@
 package excercise;
 
-public class BankAccount {
+class InvalidWithdraw extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidWithdraw(String msg) {
+		super(msg);
+	}
+}
 
+public class BankAccount {
+	
+	int balance;
+	
+	public void deposit (int money) {
+		
+	}
+	
+	public void withdraw(int money) {
+		
+	}
+	
+	
 }
