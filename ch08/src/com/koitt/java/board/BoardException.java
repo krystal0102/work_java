958d5c9a4a6f9a95ebb76f534e3f768868b99913
KOITT <KOITT@KOITT-PC13>
2018-01-17 17:43:25 +0900

    게시판 프로그램 수정수정

--- ch08/src/com/koitt/java/board/BoardException.java
+++ ch08/src/com/koitt/java/board/BoardException.java
@@ -1,7 +1,9 @@
 package com.koitt.java.board;
 
+//1. 클래스 생성 후 Exception 상속받기
 public class BoardException extends Exception {
+	
 	public BoardException(String msg) {
-		super(msg);	
- 	}
+		super(msg);
+	}
 }
