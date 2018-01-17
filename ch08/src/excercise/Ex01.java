5fd1fd828037046f171c5084b00ebfd12b82c105
KOITT <KOITT@KOITT-PC13>
2018-01-17 17:44:24 +0900

    ch08 프로그래밍 연습

---
+++ ch08/src/excercise/Ex01.java
@@ -0,0 +1,27 @@
+package excercise;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		int x = 0, y= 0, z = 0;
+		Scanner input = new Scanner(System.in);
+		
+
+		try {
+
+			System.out.print("정수 두 개 입력: ");
+			x = input.nextInt();
+			y = input.nextInt();
+			z = x * y;
+			
+			System.out.printf("%d * %d = %d %n", x, y, z);
+		}
+		catch (InputMismatchException e) {
+			System.out.println("정수로만 입력해주세요~~");
+		}
+	}
+
+}
