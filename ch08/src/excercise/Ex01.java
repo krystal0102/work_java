9bb9b1955f7feb6cff7fe4199bca356cab05fbf4
KOITT <KOITT@KOITT-PC13>
2018-01-18 13:59:14 +0900

    ch08 프로그래밍 연습 1번 풀이 완료

--- ch08/src/excercise/Ex01.java
+++ ch08/src/excercise/Ex01.java
@@ -8,20 +8,23 @@ public class Ex01 {
 	public static void main(String[] args) {
 		int x = 0, y= 0, z = 0;
 		Scanner input = new Scanner(System.in);
+		System.out.print("정수 두 개 입력: ");
 		
-
 		try {
-
-			System.out.print("정수 두 개 입력: ");
+			
 			x = input.nextInt();
 			y = input.nextInt();
 			z = x * y;
 			
 			System.out.printf("%d * %d = %d %n", x, y, z);
+			
 		}
+		
 		catch (InputMismatchException e) {
 			System.out.println("정수로만 입력해주세요~~");
 		}
+		
+		input.close();
 	}
 
 }
