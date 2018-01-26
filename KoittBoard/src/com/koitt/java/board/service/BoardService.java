46f569d00b4687a7cbe964a937368e79477452c5
KOITT <KOITT@KOITT-PC13>
2018-01-26 17:03:14 +0900

    SQL 게시판 프로그램 BoardService에 count 삭제

--- KoittBoard/src/com/koitt/java/board/service/BoardService.java
+++ KoittBoard/src/com/koitt/java/board/service/BoardService.java
@@ -14,17 +14,13 @@ public class BoardService {
 	public static final String COUNT_FILE_NAME = "board-count.dat";
 	
 	private BoardDao dao;
-	private Integer count;	// 기본값이 null 이기 때문에 생성자에서 0으로 초기화해야 한다.
 	
 	public BoardService() {
 		this.dao = new BoardDao();
-		this.count = this.dao.lastBoardId();	// 0으로 초기화
 	}
 	
 								// 3.
 	public void add(Board board) throws BoardException, SQLException {
-		board.setId(++this.count);		// null값이었던 id값을 채워준다.
-		board.setRegDate(new Date());	// new Date() 하는 순간의 시간이 저장된다.
 		this.dao.insert(board);			// 자료구조(ArrayList)에 저장하기 위해 dao로 board 객체를 전달
 	}
 	
